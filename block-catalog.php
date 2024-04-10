7e1588fd2648a8a96250722fdfb37cd0daa186de
Jeffrey Paul <jeffpaul@hotmail.com>
2024-04-10 16:29:54 -0500

    bump WordPress "tested up to" version 6.5
    Co-Authored-By: Sudip Dadhaniya <94599521+sudip-md@users.noreply.github.com>

--- block-catalog.php
+++ block-catalog.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Block Catalog
  * Description:       Easily keep track of which Gutenberg Blocks are used across your site.
  * Version:           1.5.4
- * Requires at least: 5.7
+ * Requires at least: 6.3
  * Requires PHP:      7.4
  * Author:            10up
  * Author URI:        https://10up.com
